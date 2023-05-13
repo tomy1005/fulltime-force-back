c5db3ceb554646c5baa6ad2ddcab7f7d52bf55cb
tomas leonangeli <tomyleonangeli@gmail.com>
2023-05-13 14:02:44 -0300

    create end poit to fetch data from git

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('history')
-  getHello(): string {
-    return this.appService.getHello();
+  getGitHistory() {
+    return this.appService.gitHistory();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,23 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosError, AxiosResponse } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly httpService: HttpService) { }
+
+  async gitHistory(): Promise<any> {
+    const { data } = await firstValueFrom(
+      this.httpService.get<any[]>('https://api.github.com/repos/tomy1005/fulltime-force-front/commits', { headers: { 'auth': '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' } }).pipe(
+        catchError((error: AxiosError) => {
+          throw 'An error happened!';
+        }),
+      ),
+    );
+    return data;
+  }
   getHello(): string {
     return 'Hello World!';
   }
